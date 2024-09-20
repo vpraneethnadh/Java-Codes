dbb4c4841de90cc7571494e23ed0bf1fb9702da3
Vudattu Praneethnadh <vpraneethnadh@gmail.com>
2024-09-20 12:55:39 +0530

    Added FibonacciNumber.java file

---
+++ FibonacciNumber.java
@@ -0,0 +1,14 @@
+public class FibonacciNumber {
+    public static int fib(int n) {
+        if (n == 0 || n == 1)
+            return 0;
+        else if (n == 2)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(fib(5));
+    }
+}
