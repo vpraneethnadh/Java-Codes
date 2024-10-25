24f9b51195a7e4bc852d7e6e904c6e7e80ce73a0
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2024-10-25 19:58:10 +0530

    Add files via upload

---
+++ CircularArrangement.java
@@ -0,0 +1,58 @@
+// An international round table conference will be held in india. Presidents from all over the world representing their respective 
+// countries will be attending the conference. The task is to find the possible number of ways(P) to make the N members sit around the 
+// circular table such that.
+// The president and prime minister of India will always sit next to each other.
+
+// Example 1:
+// Input :
+// 4   -> Value of N(No. of members)
+// Output : 
+// 12  -> Possible ways of seating the members
+
+// Explanation:
+// 2  members should always be next to each other. 
+// So, 2 members can be in 2!ways
+// Rest of the members can be arranged in (4-1)! ways.(1 is subtracted because the previously selected two members will be considered 
+// as single members now).
+// So total possible ways 4 members can be seated around the circular table 2*6= 12.
+// Hence, output is 12.
+
+// Example 2:
+// Input:
+// 10  -> Value of N(No. of members)
+// Output :
+// 725760 -> Possible ways of seating the members 
+
+// Explanation:
+// 2 members should always be next to each other.
+// So, 2 members can be in 2! ways 
+// Rest of the members can be arranged in (10-1)! Ways. (1 is subtracted because the previously selected two members will be considered as a single member now).
+// So, total possible ways 10 members can be seated around a round table is 
+// 2*362880 = 725760 ways.
+// Hence, output is 725760.
+// The input format for testing
+// The candidate has to write the code to accept one input 
+// First input – Accept value of number of N(Positive integer number)
+// The output format for testing 
+// The output should be a positive integer number or print the message(if any) given in the problem statement(Check the output in example 1, example2)
+
+// Constraints :
+// 2<=N<=50
+
+public class CircularArrangement {
+    public static void solve(int n) {
+        if (n == 0 || n == 1 || n == 2)
+            System.out.println(n);
+        else {
+            int x = 1;
+            for (int i = 1; i < n; i++)
+                x *= i;
+            System.out.println(2 * x);
+        }
+    }
+
+    public static void main(String args[]) {
+        solve(10);
+        solve(4);
+    }
+}
